8a5f99de1d164e64c9b58c04199f9a2d06601696
hota1024 <hotalog@hota1024.com>
2021-05-09 21:35:29 +0900

    ✨ impl animation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,27 @@
 <template>
-  <div>
+  <transition name="page">
     <Nuxt />
-  </div>
+  </transition>
 </template>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
+html,
+body {
   margin: 0;
+  padding: 0;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+* {
+  box-sizing: border-box;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+.page-enter {
+  /* opacity: 0; */
 }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.page-enter-active {
+  /* transition: opacity 2s; */
 }
 </style>
