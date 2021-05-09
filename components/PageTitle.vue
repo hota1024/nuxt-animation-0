86a1b9095a83545222fece112b72512a0cb1e225
hota1024 <hotalog@hota1024.com>
2021-05-09 21:35:36 +0900

    ✨ impl

---
+++ components/PageTitle.vue
@@ -0,0 +1,77 @@
+<template>
+  <h1 class="title">
+    <span
+      v-for="(c, i) in title"
+      :key="i"
+      class="title-char"
+      :style="charStyle(i)"
+      >{{ c }}</span
+    >
+  </h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {
+    charStyle(i: number) {
+      return {
+        animation: `title-char-in 0.6s cubic-bezier(0,.7,.3,1) both ${
+          0.2 + i * 0.02
+        }s`,
+      }
+    },
+  },
+})
+</script>
+
+<style scoped>
+.title {
+  /* Font */
+  font-size: 4rem;
+  color: #202040;
+
+  /* Background */
+  background-image: linear-gradient(135deg, #63aaf8, #4481e2);
+  background-repeat: no-repeat;
+  background-position: 10px bottom;
+  padding-right: 10px;
+
+  /* Animation */
+  animation: title-background-in 1s cubic-bezier(0.6, 0.2, 0, 1) both;
+}
+
+.title-char {
+  opacity: 0;
+  display: inline-block;
+}
+</style>
+
+<style>
+@keyframes title-background-in {
+  0% {
+    background-size: 0 30%;
+  }
+  100% {
+    background-size: 100% 30%;
+  }
+}
+
+@keyframes title-char-in {
+  0% {
+    transform: translateY(50px) rotate(6deg);
+    opacity: 0;
+  }
+  100% {
+    transform: translate(0) rotate(0.1deg);
+    opacity: 1;
+  }
+}
+</style>
---
+++ components/Thumbnail.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="thumbnail">
+    <img :src="src" width="100%" height="100%" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    src: {
+      type: String,
+      default: null,
+    },
+  },
+})
+</script>
+
+<style scoped>
+.thumbnail {
+  overflow: hidden;
+  max-width: 640px;
+  height: 100%;
+  border-radius: 8px;
+  box-shadow: 4px 4px 32px rgba(0, 0, 0, 0.2);
+
+  /* Animation */
+  animation: thumbnail-in 1.2s cubic-bezier(0, 0.7, 0.3, 1) both;
+}
+
+.thumbnail img {
+  animation: thumbnail-zoom-out 1.2s cubic-bezier(0, 0.7, 0.3, 1) both;
+}
+</style>
+
+<style>
+@keyframes thumbnail-in {
+  0% {
+    transform: translateY(50px);
+  }
+  100% {
+    transform: translate(0);
+  }
+}
+
+@keyframes thumbnail-zoom-out {
+  0% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
